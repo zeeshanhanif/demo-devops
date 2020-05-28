a55614928bf714aa03fd2e4c263c590ceb94bb5e
zeeshanhanif <zeeshanhanif@gmail.com>
2020-05-28 17:54:01 +0500

    updated react app

--- 03_reactapp/src/App.js
+++ 03_reactapp/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.<br/>
-          Update
+          Update app
         </p>
         <a
           className="App-link"
