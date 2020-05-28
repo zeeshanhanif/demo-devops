f4ca87ac9fb3e4d25e03d1047d6b700ed551c37b
zeeshanhanif <zeeshanhanif@gmail.com>
2020-05-28 18:58:27 +0500

    updated deployment and apps

--- 03_reactapp/src/App.js
+++ 03_reactapp/src/App.js
@@ -9,7 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.<br/>
-          Update app
+          Update app<br/>
+          03 React App on Surge
         </p>
         <a
           className="App-link"
--- 04_reactapp/src/App.js
+++ 04_reactapp/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.<br/>
-          Updated
+          04 React App on Heroku
         </p>
         <a
           className="App-link"
