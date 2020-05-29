c84035e03644b851b4e7399890d14e42d0a8a0af
zeeshanhanif <zeeshanhanif@gmail.com>
2020-05-29 10:58:57 +0500

    updated app 4

--- 04_reactapp/src/App.js
+++ 04_reactapp/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.<br/>
           04 React App on Heroku <br/>
-          Finally Deployed on Heorku
+          Finally Deployed on Heorku updated
         </p>
         <a
           className="App-link"
